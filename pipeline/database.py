1784c6d23bb8a30c65013ea0498b3a24545c2bf5
oscarl77 <oscarlaurens18@gmail.com>
2026-07-11 18:58:39 +0100

    Refactored from DuckDB to PostgreSQL

--- pipeline/database.py
+++ pipeline/database.py
@@ -1,22 +1,21 @@
-import duckdb
-from pipeline import config
+import psycopg2
+from psycopg2.extras import execute_values
+from pipeline.config import DATABASE_URL
 
 def initialise_db():
     """Establishes the target file and ensures the telemetry table structure exists."""
-    conn = duckdb.connect(str(config.DB_PATH))
-    
-    # Generate the columnar time-series table structure
-    conn.execute("""
-        CREATE TABLE IF NOT EXISTS tram_telemetry (
-            route_id VARCHAR,
-            vehicle_id VARCHAR,
-            latitude DOUBLE,
-            longitude DOUBLE,
-            timestamp BIGINT,
-        );
-    """)
-    conn.close()
-    print(f"Database initialized cleanly at: {config.DB_PATH}")
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS tram_telemetry (
+                    id SERIAL PRIMARY KEY,
+                    route_id VARCHAR(50),
+                    vehicle_id VARCHAR(50),
+                    latitude DOUBLE PRECISION,
+                    longitude DOUBLE PRECISION,
+                    timestamp BIGINT
+                );
+            """)
 
 def insert_telemetry_batch(records):
     """Inserts a collection of parsed position dictionaries into the storage file.
@@ -25,22 +24,23 @@ def insert_telemetry_batch(records):
     """
     if not records:
         return
-        
-    conn = duckdb.connect(str(config.DB_PATH))
-    
     # Convert the list of Python dicts directly into a DuckDB internal relation
     # and append it in a single highly-optimized transactional block
-    try:
-        # Transform our array of dictionaries into a clean matrix of raw positional values
-        data_matrix = [[r['route_id'], r['vehicle_id'], r['latitude'], r['longitude'], r['timestamp']] for r in records]
-        
-        # Using executemany tells the engine to map the 5 placeholders 
-        # to each individual array entry across the entire collection sequentially.
-        conn.executemany("""
-            INSERT INTO tram_telemetry (route_id, vehicle_id, latitude, longitude, timestamp)
-            VALUES (?, ?, ?, ?, ?)
-        """, data_matrix)
-    except Exception as e:
-        print(f"Database Write Failure: {e}")
-    finally:
-        conn.close()
+    values = [
+    (
+        r.get("route_id"),
+        r.get("vehicle_id"),
+        r.get("latitude"),
+        r.get("longitude"),
+        r.get("timestamp")
+    )
+    for r in records
+    ]
+    query = """
+        INSERT INTO tram_telemetry (route_id, vehicle_id, latitude, longitude, timestamp)
+        VALUES %s;
+    """
+    # Using context managers ('with') automatically commits transactions and closes connections/cursors
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cursor:
+            execute_values(cursor, query, values)
